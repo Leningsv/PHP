21487f204a746b2c23aa7a3241ea336ac4af40de
Lenin <leningsv@hotmail.com>
2016-07-18 17:55:28 -0500

    Git

--- PHPTutorialObjetos/PHP7Tutorial7MetodosYAtributosStatic/index.php
+++ PHPTutorialObjetos/PHP7Tutorial7MetodosYAtributosStatic/index.php
@@ -10,12 +10,35 @@
         // Metodos
         public function Bienvenida()
         {
-            echo 'Bienvenidos a <b>'.nombre
+            echo 'Bienvenidos a <b>'.$this->nombre.'</b> la pagina es <b>'.self::$url.'</b><br>';
+            echo 'Bienvenidos a <b>'.$this->nombre.'</b> la pagina es <b>'.Pagina::$url.'</b><br>';
+        }
+        public function Bienvenida2()
+        {
+            echo 'Bienvenidos a'.self::$url;
         }
         function __construct()
         {
             # code...
         }
     }
+    /**
+     * 
+     */
+    class Web extends Pagina
+    {
+        
+        function __construct()
+        {
+            # code...
+        }
+    }
+    
+    $pagina =new Pagina();
+    $pagina ->Bienvenida();
+    Pagina :: Bienvenida2();
+    Web :: Bienvenida2();
+    
+
     
 ?>
