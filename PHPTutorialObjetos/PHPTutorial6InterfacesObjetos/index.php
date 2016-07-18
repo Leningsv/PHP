20d246465039c6168d38abd63b7d1af863db315c
Lenin <leningsv@hotmail.com>
2016-07-18 18:06:13 -0500

    comit

--- PHPTutorialObjetos/PHPTutorial6InterfacesObjetos/index.php
+++ PHPTutorialObjetos/PHPTutorial6InterfacesObjetos/index.php
@@ -12,11 +12,40 @@
      */
     class Deportivo implements Gasolina
     {
-        
-        function __construct(argument)
+        private $estado = false;
+        private $tanque = 0;
+        //Atributos
+        public function Estado()
+        {
+            # code...
+        }
+        public function Ver()
+        {
+            echo 'Hola';
+        }
+        public function Encender()
+        {
+            # code...
+        }
+        public function Apagar()
+        {
+            # code...
+        }
+        public function VaciarTanque()
+        {
+            # code...
+        }
+        public function LlenarTanque()
+        {
+            # code...
+        }
+        //Metodos
+        function __construct()
         {
             # code...
         }
     }
+    $obj= new Deportivo();
+    obj -> ver()
     
 ?>
